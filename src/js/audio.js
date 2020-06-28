f289e16bf3bb9fb8b194f7f3f2bd5f87bbc969c0
Rainner Lins <rainnerlins@gmail.com>
2020-06-28 02:37:30 -0400

    Check for AudioContext support, added support for resuming suspended audio in Chrome.

--- src/js/audio.js
+++ src/js/audio.js
@@ -2,15 +2,21 @@
  * Audio handler object
  */
 export default {
-  _audio: new Audio(),
-  _context: new AudioContext(),
-  _freq: new Uint8Array(),
+  _context: null,
+  _audio: null,
+  _freq: null,
   _source: null,
   _gain: null,
   _analyser: null,
 
   // setup audio routing
   setupAudio() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext || false;
+    if ( !AudioContext ) return false;
+
+    this._audio = new Audio();
+    this._freq = new Uint8Array( 1024 );
+    this._context = new AudioContext();
     this._source = this._context.createMediaElementSource( this._audio );
     this._analyser = this._context.createAnalyser();
     this._gain = this._context.createGain();
@@ -26,9 +32,18 @@ export default {
     return this._audio;
   },
 
+  // get audio context state
+  getState( state ) {
+    if ( !this._context ) return '';
+    if ( state ) return ( this._context.state === state );
+    return this._context.state;
+  },
+
   // update and return analyser frequency data
   getFreqData() {
-    this._analyser.getByteFrequencyData( this._freq );
+    if ( this._analyser ) {
+      this._analyser.getByteFrequencyData( this._freq );
+    }
     return this._freq;
   },
 
@@ -41,18 +56,28 @@ export default {
     this._gain.gain.value = volume;
   },
 
+  // stop playing audio
+  stopAudio() {
+    if ( !this._audio ) return;
+    try { this._audio.pause(); } catch ( e ) {}
+    try { this._audio.stop(); } catch ( e ) {}
+    try { this._audio.close(); } catch ( e ) {}
+  },
+
+  // resume suspended audio in chrome
+  resumeAudio( callback ) {
+    if ( !this._context ) return;
+    this._context.resume().then( callback ).catch( e => {} );
+  },
+
   // play audio source url
   playSource( source ) {
+    if ( !this._audio ) return;
     this.stopAudio();
+    this.resumeAudio();
     this._audio.src = String( source || '' ) + '?x=' + Date.now();
     this._audio.crossOrigin = 'anonymous';
     this._audio.load();
   },
 
-  // stop playing audio
-  stopAudio() {
-    try { this._audio.pause(); } catch ( e ) {}
-    try { this._audio.stop(); } catch ( e ) {}
-    try { this._audio.close(); } catch ( e ) {}
-  },
 }
